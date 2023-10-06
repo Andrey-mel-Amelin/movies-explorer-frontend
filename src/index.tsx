17af699b7868ed6f07c4637ffd854e073341ff4e
Амелин Андрей Александрович <amelin_a_qschool@qsoft.ru>
2023-10-06 04:07:45 +0300

    fix: vercel

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
-require('dotenv').config();
 
 const router = createBrowserRouter([
   {
